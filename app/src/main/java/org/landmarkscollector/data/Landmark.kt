d7e3ee765439ed4e77aae747b7976af789744e90
ext.zshakirov <ext.zshakirov@tinkoff.ru>
2023-04-03 22:30:51 +0600

    Experimental ticker and Model added

---
+++ app/src/main/java/org/landmarkscollector/data/Landmark.kt
@@ -0,0 +1,54 @@
+package org.landmarkscollector.data
+
+sealed class Landmark {
+    abstract val landmarkType: String
+    abstract val landmarkNumber: String
+    abstract val x: Float
+    abstract val y: Float
+    abstract val z: Float
+
+    sealed class Hand(
+        override val landmarkNumber: String,
+        override val x: Float,
+        override val y: Float,
+        override val z: Float,
+        handedness: String,
+    ) : Landmark() {
+
+        override val landmarkType: String = "${handedness}_hand"
+
+        class Left(
+            override val landmarkNumber: String,
+            override val x: Float,
+            override val y: Float,
+            override val z: Float
+        ) : Hand(landmarkNumber, x, y, z, "left")
+
+        class Right(
+            override val landmarkNumber: String,
+            override val x: Float,
+            override val y: Float,
+            override val z: Float
+        ) : Hand(landmarkNumber, x, y, z, "right")
+    }
+
+    class Face(
+        override val landmarkNumber: String,
+        override val x: Float,
+        override val y: Float,
+        override val z: Float
+    ) : Landmark() {
+
+        override val landmarkType: String = "face"
+    }
+
+    class Pose(
+        override val landmarkNumber: String,
+        override val x: Float,
+        override val y: Float,
+        override val z: Float
+    ) : Landmark() {
+
+        override val landmarkType: String = "pose"
+    }
+}
