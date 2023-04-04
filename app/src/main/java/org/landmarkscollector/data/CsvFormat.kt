5a5c03905a6a429af7aacc75a860b3e0708431a3
ext.zshakirov <ext.zshakirov@tinkoff.ru>
2023-04-05 05:03:52 +0600

    Experimental face-pose handler added

--- app/src/main/java/org/landmarkscollector/data/CsvFormat.kt
+++ app/src/main/java/org/landmarkscollector/data/CsvFormat.kt
@@ -3,9 +3,10 @@ package org.landmarkscollector.data
 data class CsvRow(
     val frame: Int,
     val landmarkIndex: Int,
-    val rowId: String,
     val type: String,
     val x: Float?,
     val y: Float?,
     val z: Float?
-)
+) {
+    val rowId = "${frame}_${type}_${landmarkIndex}"
+}
