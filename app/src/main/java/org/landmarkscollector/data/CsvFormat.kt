2fffb84cf24fcd72ddf60319e9030c2ecdc6d068
ext.zshakirov <ext.zshakirov@tinkoff.ru>
2023-04-07 18:08:25 +0600

    Experimental normalized values collecting is done

--- app/src/main/java/org/landmarkscollector/data/CsvFormat.kt
+++ app/src/main/java/org/landmarkscollector/data/CsvFormat.kt
@@ -3,10 +3,18 @@ package org.landmarkscollector.data
 data class CsvRow(
     val frame: Int,
     val landmarkIndex: Int,
-    val type: String,
+    val type: Landmark.LandmarkType,
     val x: Float?,
     val y: Float?,
     val z: Float?
 ) {
-    val rowId = "${frame}_${type}_${landmarkIndex}"
+    val rowId: String = type.getRowId(frame, landmarkIndex)
+
+    companion object {
+
+        fun Landmark.LandmarkType.getRowId(
+            frameNumber: Int,
+            landmarkIndex: Int,
+        ): String = "$frameNumber-$typeName-$landmarkIndex"
+    }
 }
