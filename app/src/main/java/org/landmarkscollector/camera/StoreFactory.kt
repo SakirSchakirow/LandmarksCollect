5cdbd54926c6192d8fe634efb7c70f4ee6f47d15
SakirSchakirow <sakir.schakirow@gmail.com>
2023-04-21 22:26:38 +0600

    File Naming and csv-fields naming corrections

--- app/src/main/java/org/landmarkscollector/camera/StoreFactory.kt
+++ app/src/main/java/org/landmarkscollector/camera/StoreFactory.kt
@@ -3,7 +3,7 @@ package org.landmarkscollector.camera
 import android.content.Context
 import org.landmarkscollector.domain.repository.ExportRepositoryImpl
 import org.landmarkscollector.domain.repository.csv.CsvFileCreator
-import org.landmarkscollector.domain.repository.csv.DataConverterCSV
+import org.landmarkscollector.domain.repository.csv.DataConverterCsv
 import org.landmarkscollector.domain.repository.file.AndroidInternalStorageFileWriter
 import vivid.money.elmslie.core.store.ElmStore
 
@@ -13,7 +13,7 @@ fun storeFactory(context: Context) = ElmStore(
     actor = Actor(
         exportRepository = ExportRepositoryImpl(
             fileWriter = AndroidInternalStorageFileWriter(context),
-            dataConverter = DataConverterCSV()
+            dataConverter = DataConverterCsv()
         )
     )
 )
