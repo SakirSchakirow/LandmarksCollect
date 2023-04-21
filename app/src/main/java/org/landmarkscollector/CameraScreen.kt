3202224c705d8e16f450cbfcc007b7e162c40626
SakirSchakirow <sakir.schakirow@gmail.com>
2023-04-21 22:26:37 +0600

    Experimental setting with hands

---
+++ app/src/main/java/org/landmarkscollector/CameraScreen.kt
@@ -0,0 +1,144 @@
+package org.landmarkscollector
+
+import android.Manifest
+import android.util.Log
+import android.view.Surface
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionRequired
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.mediapipe.tasks.vision.core.RunningMode
+import org.landmarkscollector.hands.HandLandmarkerHelper
+import org.landmarkscollector.hands.LandmarkerListener
+import org.landmarkscollector.hands.OverlayView
+import org.landmarkscollector.hands.ResultBundle
+import java.util.concurrent.Executors
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun CameraScreen() {
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    // Camera permission state
+    val cameraPermissionState = rememberPermissionState(
+        Manifest.permission.CAMERA
+    )
+
+    val previewView: PreviewView = remember {
+        PreviewView(context)
+            .apply { scaleType = PreviewView.ScaleType.FILL_START }
+    }
+
+    val overlayView: OverlayView = remember {
+        OverlayView(context)
+    }
+
+    val cameraSelector: MutableState<CameraSelector> = remember {
+        mutableStateOf(CameraSelector.DEFAULT_FRONT_CAMERA)
+    }
+
+    LaunchedEffect(previewView) {
+        lateinit var handLandmarkerHelper: HandLandmarkerHelper
+        val backgroundExecutor = Executors.newSingleThreadExecutor()
+        backgroundExecutor.execute {
+            handLandmarkerHelper = HandLandmarkerHelper(context, object : LandmarkerListener {
+
+                override fun onError(error: String) {
+                    Log.e("CameraScreen", error)
+                }
+
+                override fun onResults(resultBundle: ResultBundle) {
+                    ContextCompat.getMainExecutor(context).execute {
+                        with(overlayView) {
+                            setResults(
+                                handLandmarkerResults = resultBundle.result,
+                                imageHeight = resultBundle.inputImageHeight,
+                                imageWidth = resultBundle.inputImageWidth,
+                                runningMode = RunningMode.LIVE_STREAM
+                            )
+                            invalidate()
+                        }
+                    }
+                }
+            })
+        }
+
+        with(suspendCoroutine { continuation ->
+            ProcessCameraProvider.getInstance(context).also { future ->
+                future.addListener(
+                    {
+                        continuation.resume(future.get())
+                    },
+                    ContextCompat.getMainExecutor(context)
+                )
+            }
+        }) {
+            unbindAll()
+            bindToLifecycle(
+                lifecycleOwner,
+                cameraSelector.value,
+                Preview.Builder()
+                    .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                    .setTargetRotation(previewView.display.rotation)
+                    .build()
+                    .apply { setSurfaceProvider(previewView.surfaceProvider) },
+                ImageAnalysis.Builder().setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                    .setTargetRotation(previewView.display.rotation)
+                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                    .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                    .build()
+                    // The analyzer can then be assigned to the instance
+                    .also {
+
+                        it.setAnalyzer(backgroundExecutor) { imageProxy ->
+                            handLandmarkerHelper.detectLiveStream(
+                                imageProxy = imageProxy,
+                                isFrontCamera = cameraSelector.value == CameraSelector.DEFAULT_FRONT_CAMERA
+                            )
+                        }
+                    }
+            )
+        }
+    }
+
+    PermissionRequired(
+        permissionState = cameraPermissionState,
+        permissionNotGrantedContent = {
+            Button(onClick = { cameraPermissionState.launchPermissionRequest() }) {
+                Text("Request permission")
+            }
+        },
+        permissionNotAvailableContent = { /* ... */ }
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            AndroidView(
+                factory = { previewView },
+                modifier = Modifier.fillMaxSize()
+            )
+            AndroidView(
+                factory = { overlayView },
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+    }
+}
---
+++ app/src/main/java/org/landmarkscollector/hands/HandLandmarkerHelper.kt
@@ -0,0 +1,166 @@
+package org.landmarkscollector.hands
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.os.SystemClock
+import android.util.Log
+import androidx.annotation.VisibleForTesting
+import androidx.camera.core.ImageProxy
+import com.google.mediapipe.framework.image.BitmapImageBuilder
+import com.google.mediapipe.framework.image.MPImage
+import com.google.mediapipe.tasks.core.BaseOptions
+import com.google.mediapipe.tasks.core.Delegate
+import com.google.mediapipe.tasks.vision.core.RunningMode
+import com.google.mediapipe.tasks.vision.handlandmarker.HandLandmarker
+import com.google.mediapipe.tasks.vision.handlandmarker.HandLandmarkerResult
+
+private const val MP_HAND_LANDMARKER_TASK = "hand_landmarker.task"
+private const val TAG = "HandLandmarkerHelper"
+
+class HandLandmarkerHelper(
+    val context: Context,
+    val landmarkerListener: LandmarkerListener
+) {
+
+    private var handLandmarker: HandLandmarker? = null
+
+    init {
+        setupHandLandmarker()
+    }
+
+    fun clearHandLandmarker() {
+        handLandmarker?.close()
+        handLandmarker = null
+    }
+
+    fun isClose(): Boolean {
+        return handLandmarker == null
+    }
+
+    // Initialize the Hand landmarker using current settings on the
+    // thread that is using it. CPU can be used with Landmarker
+    // that are created on the main thread and used on a background thread, but
+    // the GPU delegate needs to be used on the thread that initialized the
+    // Landmarker
+    fun setupHandLandmarker() {
+        // Set general hand landmarker options
+        val baseOptionBuilder = BaseOptions.builder()
+            .setDelegate(Delegate.CPU)
+            .setModelAssetPath(MP_HAND_LANDMARKER_TASK)
+
+        try {
+            val baseOptions = baseOptionBuilder.build()
+            // Create an option builder with base options and specific
+            // options only use for Hand Landmarker.
+            val optionsBuilder =
+                HandLandmarker.HandLandmarkerOptions.builder()
+                    .setBaseOptions(baseOptions)
+                    .setMinHandDetectionConfidence(0.5F)
+                    .setMinTrackingConfidence(0.5F)
+                    .setMinHandPresenceConfidence(0.5F)
+                    .setNumHands(2)
+                    .setRunningMode(RunningMode.LIVE_STREAM)
+                    .setResultListener(this::returnLivestreamResult)
+                    .setErrorListener(this::returnLivestreamError)
+
+            val options = optionsBuilder.build()
+            handLandmarker = HandLandmarker.createFromOptions(context, options)
+        } catch (e: IllegalStateException) {
+            landmarkerListener.onError(
+                "Hand Landmarker failed to initialize. See error logs for " +
+                        "details"
+            )
+            Log.e(
+                TAG, "MediaPipe failed to load the task with error: " + e
+                    .message
+            )
+        } catch (e: RuntimeException) {
+            // This occurs if the model being used does not support GPU
+            landmarkerListener.onError(
+                "Hand Landmarker failed to initialize. See error logs for " +
+                        "details"
+            )
+            Log.e(
+                TAG,
+                "Image classifier failed to load model with error: " + e.message
+            )
+        }
+    }
+
+    // Convert the ImageProxy to MP Image and feed it to HandlandmakerHelper.
+    fun detectLiveStream(
+        imageProxy: ImageProxy,
+        isFrontCamera: Boolean
+    ) {
+        val frameTime = SystemClock.uptimeMillis()
+
+        // Copy out RGB bits from the frame to a bitmap buffer
+        val bitmapBuffer =
+            Bitmap.createBitmap(
+                imageProxy.width,
+                imageProxy.height,
+                Bitmap.Config.ARGB_8888
+            )
+        imageProxy.use { bitmapBuffer.copyPixelsFromBuffer(imageProxy.planes[0].buffer) }
+        imageProxy.close()
+
+        val matrix = Matrix().apply {
+            // Rotate the frame received from the camera to be in the same direction as it'll be shown
+            postRotate(imageProxy.imageInfo.rotationDegrees.toFloat())
+
+            // flip image if user use front camera
+            if (isFrontCamera) {
+                postScale(
+                    -1f,
+                    1f,
+                    imageProxy.width.toFloat(),
+                    imageProxy.height.toFloat()
+                )
+            }
+        }
+        val rotatedBitmap = Bitmap.createBitmap(
+            bitmapBuffer, 0, 0, bitmapBuffer.width, bitmapBuffer.height,
+            matrix, true
+        )
+
+        // Convert the input Bitmap object to an MPImage object to run inference
+        val mpImage = BitmapImageBuilder(rotatedBitmap).build()
+
+        detectAsync(mpImage, frameTime)
+    }
+
+    // Run hand hand landmark using MediaPipe Hand Landmarker API
+    @VisibleForTesting
+    fun detectAsync(mpImage: MPImage, frameTime: Long) {
+        handLandmarker?.detectAsync(mpImage, frameTime)
+        // As we're using running mode LIVE_STREAM, the landmark result will
+        // be returned in returnLivestreamResult function
+    }
+
+    // Return the landmark result to this HandLandmarkerHelper's caller
+    private fun returnLivestreamResult(
+        result: HandLandmarkerResult,
+        input: MPImage
+    ) {
+        val finishTimeMs = SystemClock.uptimeMillis()
+        val inferenceTime = finishTimeMs - result.timestampMs()
+
+        landmarkerListener.onResults(
+            ResultBundle(
+                result,
+                inferenceTime,
+                input.height,
+                input.width
+            )
+        )
+    }
+
+    // Return errors thrown during detection to this HandLandmarkerHelper's
+    // caller
+    private fun returnLivestreamError(error: RuntimeException) {
+        landmarkerListener.onError(
+            error.message ?: "An unknown error has occurred"
+        )
+    }
+}
